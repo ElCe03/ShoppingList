0328d597825d6d5a1754dda37ff65cabd3fe09cf
ElCe03 <76115371+ElCe03@users.noreply.github.com>
2023-06-08 13:13:06 +0200

    Shopping list v2

--- app/src/main/java/com/uni/esame/MainActivity.kt
+++ app/src/main/java/com/uni/esame/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ListView
+import android.widget.NumberPicker
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 
@@ -28,37 +29,28 @@ class MainActivity : AppCompatActivity() {
 
         val addButton = findViewById<Button>(R.id.addButton)
         val itemNameEditText = findViewById<EditText>(R.id.itemNameEditText)
-        val itemQuantityEditText = findViewById<EditText>(R.id.itemQuantityEditText)
+        val itemQuantityPicker = findViewById<NumberPicker>(R.id.itemQuantityPicker)
+
+        itemQuantityPicker.minValue = 0
+        itemQuantityPicker.maxValue = 100
 
         addButton.setOnClickListener {
             val itemName = itemNameEditText.text.toString()
-            val itemQuantity = itemQuantityEditText.text.toString().toIntOrNull()
+            val itemQuantity = itemQuantityPicker.value
 
-            if (!itemName.isNullOrEmpty() && itemQuantity != null) {
+            if (!itemName.isNullOrEmpty()) {
                 val newItem = Item(itemName, itemQuantity)
                 list.add(newItem)
                 adapter.notifyDataSetChanged()
 
                 itemNameEditText.text.clear()
-                itemQuantityEditText.text.clear()
 
                 if (list.isEmpty()) {
-                    showWelcomePopup()
+                    Toast.makeText(this, "WELCOME TO YOUR SHOPPING LIST", Toast.LENGTH_SHORT).show()
                 }
             } else {
                 Toast.makeText(this, "Please enter valid item details", Toast.LENGTH_SHORT).show()
             }
         }
-    }
-
-    private fun showWelcomePopup() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Welcome to Your Shopping List")
-        builder.setMessage("Start adding items to your list!")
-        builder.setPositiveButton("OK") { dialog, _ ->
-            dialog.dismiss()
-        }
-        val dialog = builder.create()
-        dialog.show()
     }
 }
